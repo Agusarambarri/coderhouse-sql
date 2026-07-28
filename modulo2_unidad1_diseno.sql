0425a25f911c2e962584a8ec8f1d264d9c5bbd3e
Agusarambarri <maria.agustina.arambarri@gmail.com>
2026-07-27 22:24:59 -0300

    Update modulo2_unidad1_diseno.sql

--- modulo2_unidad1_diseno.sql
+++ modulo2_unidad1_diseno.sql
@@ -1,7 +1,14 @@
-# Creando tabla de clientes
+-- Creando tabla de clientes
 CREATE TABLE clientes (
   id_cliente INT,
   nombre VARCHAR(100),
   perfil_bio TEXT,
-  fecha_registro datetime
+  fecha_registro DATE
+  )
+-- Creando tabla de productos 
+CREATE TABLE productos (
+  id_producto INT,
+  descripcion VARCHAR(255),
+  precio DECIMAL,
+  esta_activo INT
   )
